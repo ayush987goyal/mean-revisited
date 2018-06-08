334c160cc892eaedd04fd5e88d4903417ae7267e
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-08 21:40:48 +0530

    add ng material

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent implements OnInit {
-  constructor() {}
+export class PostCreateComponent {
+  newPost = 'NO CONTENT';
 
-  ngOnInit() {}
+  onAddPost() {
+    this.newPost = `The user's post`;
+  }
 }
