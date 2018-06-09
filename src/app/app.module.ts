71cad9616c6142dab68b18551de9e9fcc68bac2d
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-10 00:25:06 +0530

    add ng routing and links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import {
   MatExpansionModule
 } from '@angular/material';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,6 +25,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    AppRoutingModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
