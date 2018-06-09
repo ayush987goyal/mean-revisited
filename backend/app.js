28718e8ad537dedc90c630f8611ee3306dc9ea56
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-09 23:39:26 +0530

    add mongoose and connect to DB and add mehtods

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const Post = require('./models/post');
 
 const app = express();
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log('Connected to DB'))
+  .catch(err => console.log('Connection to DB failed'));
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'post1',
-      title: 'First server-side post',
-      content: 'This is coming from server'
-    },
-    {
-      id: 'post2',
-      title: 'Second server-side post',
-      content: 'This is coming from server!'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts
+  const posts = Post.find({}).then(docs => {
+    res.status(200).json({
+      message: 'Posts fetched successfully!',
+      posts: docs
+    });
   });
 });
 
