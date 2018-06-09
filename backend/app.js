cfa77336e3132e3fbf422a8c095c6badb7984065
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-10 01:06:59 +0530

    reorganizing api routes

--- backend/app.js
+++ backend/app.js
@@ -3,65 +3,18 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const Post = require('./models/post');
-
-mongoose
-  .connect(process.env.DB_URL)
-  .then(() => console.log('Connected to DB'))
-  .catch(err => console.log('Connection to DB failed'));
+const postsRoutes = require('./routes/posts');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: createdPost._id
-    });
-  });
-});
+app.use('/api/posts', postsRoutes);
 
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({ _id: req.params.id }, post).then(result => {
-    res.status(200).json({ message: 'Update successful!' });
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  const posts = Post.find({}).then(docs => {
-    res.status(200).json({
-      message: 'Posts fetched successfully!',
-      posts: docs
-    });
-  });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: 'Post not found!' });
-    }
-  });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    res.status(200).json({ message: 'Post deleted successfully!' });
-  });
-});
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log('Connected to DB'))
+  .catch(err => console.log('Connection to DB failed'));
 
 module.exports = app;
