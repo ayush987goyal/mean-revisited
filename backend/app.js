d9965cc04990d67c0a0f27ee084a194a1004d841
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-09 22:07:39 +0530

    Add first route

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'post1',
+      title: 'First server-side post',
+      content: 'This is coming from server'
+    },
+    {
+      id: 'post2',
+      title: 'Second server-side post',
+      content: 'This is coming from server!'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts
+  });
 });
 
 module.exports = app;
