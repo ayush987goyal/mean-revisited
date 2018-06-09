6fe0ffdd17a459c333629b81ce885c60e4e6b3fd
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-10 00:14:48 +0530

    Add delete functionality

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,12 @@ require('dotenv').config();
 
 const Post = require('./models/post');
 
-const app = express();
 mongoose
   .connect(process.env.DB_URL)
   .then(() => console.log('Connected to DB'))
   .catch(err => console.log('Connection to DB failed'));
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,9 +20,11 @@ app.post('/api/posts', (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message: 'Post added successfully'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
@@ -32,6 +34,12 @@ app.get('/api/posts', (req, res, next) => {
       message: 'Posts fetched successfully!',
       posts: docs
     });
+  });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    res.status(200).json({ message: 'Post deleted successfully!' });
   });
 });
 
