31be4129182917c4873e6a8570522e56f1525962
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-10 18:52:46 +0530

    add template for signup and route

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
+  isLoading = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  onSignup(form: NgForm) {
+    console.log(form.value);
   }
-
 }
