8753cddfe6367d2afcbba31113be0ad3ecc6ac4f
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-10 11:38:46 +0530

    Add custome mimetype validator for image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { PostsService } from '../posts.service';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -30,7 +31,7 @@ export class PostCreateComponent implements OnInit {
     this.form = this.fb.group({
       title: this.fb.control(null, [Validators.required, Validators.minLength(4)]),
       content: this.fb.control(null, [Validators.required]),
-      image: this.fb.control(null, [Validators.required])
+      image: this.fb.control(null, [Validators.required], [mimeType])
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -48,7 +49,8 @@ export class PostCreateComponent implements OnInit {
           };
           this.form.setValue({
             title: this.post.title,
-            content: this.post.content
+            content: this.post.content,
+            image: null
           });
         });
       } else {
