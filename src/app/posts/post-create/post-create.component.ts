a3329923d554a161d168ae6193cb4b0f91fb4831
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-10 11:24:45 +0530

    add image button and its preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   isLoading = false;
   form: FormGroup;
+  imagePreview: string;
   private mode = 'create';
   private postId: string;
 
@@ -28,7 +29,8 @@ export class PostCreateComponent implements OnInit {
   ngOnInit() {
     this.form = this.fb.group({
       title: this.fb.control(null, [Validators.required, Validators.minLength(4)]),
-      content: this.fb.control(null, [Validators.required])
+      content: this.fb.control(null, [Validators.required]),
+      image: this.fb.control(null, [Validators.required])
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -54,6 +56,17 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePick(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
