d9286470b571cdd2fe0062cd0ae276f82f40f40b
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-11 23:08:07 +0530

    Adding env vars in server

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const postsRoutes = require('./routes/posts');
 const userRoutes = require('./routes/user');
