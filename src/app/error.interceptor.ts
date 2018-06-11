7a11d47cc1513167e60d30089aafc22551114f22
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-11 21:59:17 +0530

    add error interceptor

---
+++ src/app/error.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log(error);
+        alert(error.error.message);
+        return throwError(error);
+      })
+    );
+  }
+}
